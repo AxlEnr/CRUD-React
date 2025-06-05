39c69e9d785d7540a9331d4793a7caaf857c5662
AxlEnr <axel.garcia0814@gmail.com>
2025-06-04 21:27:16 -0600

    First component created

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,33 +1,38 @@
+import Navbar from '../components/Navbar/Navbar';
+
 export function Index() {
   return (
-    <main className="w-screen h-screen flex items-center justify-center">
-      <div className="w-full h-full grid grid-cols-1 sm:grid-cols-2">
-        <div className="w-full h-full object-cover relative opacity-40 hover:opacity-100 transition-all">
-            <img
-                src="/assets/imgs/menLS.webp"
-                alt="Hombre"
-                className="w-full h-full object-cover "
-            />
-            <h2 
-            className="absolute inset-0 flex items-end justify-center text-white text-6xl font-bold mb-20 primaryFont">
-                Fragancias para hombre
-            </h2>
-        </div>
-        <div className="w-full h-full object-cover opacity-40 hover:opacity-100 relative transition-all">
-            <img
-                src="/assets/imgs/wmnLS.jpeg"
-                alt="Mujer"
-                className="w-full h-full object-cover "
-            />
-            <h2 
-            className="absolute inset-0 flex items-end justify-center text-white text-6xl font-bold mb-20 primaryFont">
-                Fragancias para mujer
-            </h2>
+    <main>
+      <div className='w-screen h-screen flex items-center justify-center'>
+        <div className="w-full h-full grid grid-cols-1 sm:grid-cols-2">
+          <div className="w-full h-full object-cover relative opacity-40 hover:opacity-100 transition-all">
+              <img
+                  src="/assets/imgs/menLS.webp"
+                  alt="Hombre"
+                  className="w-full h-full object-cover "
+              />
+              <h2 
+              className="absolute inset-0 flex items-end justify-center text-white text-6xl font-bold mb-20 primaryFont">
+                  Fragancias para hombre
+              </h2>
+          </div>
+          <div className="w-full h-full object-cover opacity-40 hover:opacity-100 relative transition-all">
+              <img
+                  src="/assets/imgs/wmnLS.jpeg"
+                  alt="Mujer"
+                  className="w-full h-full object-cover "
+              />
+              <h2 
+              className="absolute inset-0 flex items-end justify-center text-white text-6xl font-bold mb-20 primaryFont">
+                  Fragancias para mujer
+              </h2>
 
-        </div>
-            
-        
+          </div>
+              
+          
+        </div>        
       </div>
+
     </main>
   );
 }
