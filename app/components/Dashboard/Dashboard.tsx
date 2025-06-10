4d148eadccad396e68ba30c6bd6e00be36874b9c
= <=>
2025-06-10 01:40:39 -0600

    Dashboard and Navbar

--- app/components/Dashboard/Dashboard.tsx
+++ app/components/Dashboard/Dashboard.tsx
@@ -14,14 +14,14 @@ function Dashboard() {
   
   return (
     <aside 
-      className="shadow-lg dash-container md:h-screen md:w-25 hover:md:w-64 transition-all duration-300 ease-in-out md:fixed bg-white group"
+      className="shadow-lg dash-container md:h-screen md:w-25 hover:md:w-64 transition-all duration-300 ease-in-out md:fixed group text-black" // Cambiado a text-black
       onMouseEnter={toggleOpenNavbar}
       onMouseLeave={() => setIsOpen(!isOpen)}
     >
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex justify-between items-center py-4 md:block">
-          <div className={`${isOpen ? 'hidden' : 'block'}`}>
-            <h2 className={`text-2xl font-bold nb-name mb-6 md:mb-12 hidden md:block group-hover:md:block whitespace-nowrap overflow-hidden }`}>
+          <div className={`${!isOpen ? 'hidden' : 'block'}`}>
+            <h2 className={`sizeFont text-2xl font-bold nb-name mb-6 md:mb-12 hidden md:block group-hover:md:block whitespace-nowrap overflow-visible`}>
               Panel de Control
             </h2>
           </div>
@@ -47,29 +47,23 @@ function Dashboard() {
         </div>
 
         {/* Menú escritorio (md:block) */}
-        <nav className={`hidden md:block ${isOpen ? 'mt-20' : ''}`}>
+        <nav className={`hidden md:block ${!isOpen ? 'mt-20' : ''}`}>
           <ul className="space-y-4">
             <li>
-              <a href="#" className="flex py-2 px-3 navbar-href group-hover:px-3 w">
-                <svg viewBox="0 0 24 24" className={`transition-all duration-200 w-10`} fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M11 15C10.1183 15 9.28093 14.8098 8.52682 14.4682C8.00429 14.2315 7.74302 14.1131 7.59797 14.0722C7.4472 14.0297 7.35983 14.0143 7.20361 14.0026C7.05331 13.9914 6.94079 14 6.71575 14.0172C6.6237 14.0242 6.5425 14.0341 6.46558 14.048C5.23442 14.2709 4.27087 15.2344 4.04798 16.4656C4 16.7306 4 17.0485 4 17.6841V19.4C4 19.9601 4 20.2401 4.10899 20.454C4.20487 20.6422 4.35785 20.7951 4.54601 20.891C4.75992 21 5.03995 21 5.6 21H8.4M15 7C15 9.20914 13.2091 11 11 11C8.79086 11 7 9.20914 7 7C7 4.79086 8.79086 3 11 3C13.2091 3 15 4.79086 15 7ZM12.5898 21L14.6148 20.595C14.7914 20.5597 14.8797 20.542 14.962 20.5097C15.0351 20.4811 15.1045 20.4439 15.1689 20.399C15.2414 20.3484 15.3051 20.2848 15.4324 20.1574L19.5898 16C20.1421 15.4477 20.1421 14.5523 19.5898 14C19.0376 13.4477 18.1421 13.4477 17.5898 14L13.4324 18.1574C13.3051 18.2848 13.2414 18.3484 13.1908 18.421C13.1459 18.4853 13.1088 18.5548 13.0801 18.6279C13.0478 18.7102 13.0302 18.7985 12.9948 18.975L12.5898 21Z" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
-                </svg>
+              <a href="/usuarios" className="flex py-2 px-3 navbar-href group-hover:px-3 w">
+                <svg className={`transition-all duration-200 w-10`} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18.5 19.5L20 21M11 21H5.6C5.03995 21 4.75992 21 4.54601 20.891C4.35785 20.7951 4.20487 20.6422 4.10899 20.454C4 20.2401 4 19.9601 4 19.4V17.6841C4 17.0485 4 16.7306 4.04798 16.4656C4.27087 15.2344 5.23442 14.2709 6.46558 14.048C6.5425 14.0341 6.6237 14.0242 6.71575 14.0172C6.94079 14 7.05331 13.9914 7.20361 14.0026C7.35983 14.0143 7.4472 14.0297 7.59797 14.0722C7.74302 14.1131 8.00429 14.2315 8.52682 14.4682C9.13692 14.7446 9.8015 14.9218 10.5 14.9795M19 17.5C19 18.8807 17.8807 20 16.5 20C15.1193 20 14 18.8807 14 17.5C14 16.1193 15.1193 15 16.5 15C17.8807 15 19 16.1193 19 17.5ZM15 7C15 9.20914 13.2091 11 11 11C8.79086 11 7 9.20914 7 7C7 4.79086 8.79086 3 11 3C13.2091 3 15 4.79086 15 7Z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                 <span className="ml-1 mt-2 sizeFont hidden group-hover:inline whitespace-nowrap">Usuarios</span>
               </a>
             </li>
             <li>
-              <a href="#" className="flex py-2 px-3 navbar-href group-hover:px-3">
-                <svg className={`transition-all duration-200 w-10`} fill="#fff" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 492.005 492.006">
-                  <path d="M341.97,0c-37.549,0-89.527,21.312-106.77,50.476v-2.347c0,0-4.907,7.851-13.737,7.851v5.887h-12.758v-1.961 c0-8.674-7.027-15.701-15.701-15.701h-1.961c-8.676,0-15.701,7.027-15.701,15.701v26.81c-8.652,5.526-14.421,15.152-14.421,26.181 c0,0.874,0.188,1.693,0.261,2.544c-9.295,1.798-15.168,4.378-15.168,7.268c0,2.859,5.769,5.424,14.916,7.216 c-3.737,2.873-6.188,7.331-6.188,12.411v29.236c-50.61,15.849-87.336,63.101-87.336,118.938c0,39.758,18.69,75.1,47.692,97.918 h-0.126c0,0,47.62,38.114,47.62,44.679v6.56c0,0-3.393,2.946-14.475,9.277c-0.501,0.277-0.966,0.554-1.499,0.854 c-0.223,0.129-0.387,0.229-0.613,0.345c-0.08,0.044-0.102,0.093-0.18,0.137c-7.113,4.356-11.369,9.722-11.369,15.564 c0,14.451,25.774,26.165,57.567,26.165c31.797,0,57.566-11.714,57.566-26.165c0-4.953-3.086-9.577-8.352-13.524 c-0.242-0.689-0.87-1.411-2.238-2.177c-12.857-7.193-16.579-10.476-16.579-10.476h-0.958v-6.315l0.793-0.244 c0-6.564,47.62-44.679,47.62-44.679l-1.014,0.076c29.059-22.814,47.785-58.18,47.785-97.994 c0-55.837-36.721-103.081-87.336-118.938v-29.236c0-5.071-2.449-9.538-6.186-12.411c9.145-1.792,14.917-4.356,14.917-7.216 c0-2.881-5.871-5.47-15.168-7.268c0.07-0.857,0.258-1.671,0.258-2.544c0-11.028-5.771-20.654-14.421-26.181v-8.165h12.756v5.887 c8.833,0,13.739,7.851,13.739,7.851V89.94c17.242,29.156,69.213,50.476,106.778,50.476c45.62,0,82.613-31.432,82.613-70.204 S387.599,0,341.97,0z"></path>
-                </svg>
+              <a href="/productos" className="flex py-2 px-3 navbar-href group-hover:px-3">
+                <svg className={`transition-all duration-200 w-10`} fill="#fff" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" stroke="#fff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M450.334,287.165v-20.52c0-46.35-8.109-86.908-24.1-120.551c-13.061-27.475-31.324-50.358-54.283-68.012 c-39.216-30.153-78.579-34.451-80.235-34.62l-0.79-0.08h-74.861V16.204h-91.057v85.698h29.858v29.906H104.72v55.434H59.342 L0,294.324l56.705,201.472h227.662l56.705-201.472L281.73,187.242h-45.378v-55.434h-50.146v-29.906h29.858V74.723h73.128 c4.673,0.631,33.962,5.374,63.653,28.203c43.892,33.749,66.147,88.832,66.147,163.718v20.582 c-15.458,16.208-61.667,69.797-61.667,131.231c0,42.644,34.694,77.338,77.338,77.338S512,461.101,512,418.457 C512,356.296,465.896,303.286,450.334,287.165z M156.349,47.545h28.374V70.56h-28.374V47.545z M260.63,464.454H80.443 L37.521,311.952h46.83v70.397h172.37v-70.397h46.83L260.63,464.454z M115.692,351.009v-52.236H225.38v52.236H115.692z M263.267,218.583l34.374,62.028h-40.92v-13.18H84.351v13.18h-40.92l34.374-62.028h26.914h131.633H263.267z M205.011,163.148 v24.094H136.06v-24.094H205.011z M434.663,464.454c-25.362,0-45.996-20.634-45.996-45.996c0-42.549,29.632-82.814,46.085-101.891 c16.447,18.836,45.908,58.62,45.908,101.891C480.659,443.82,460.025,464.454,434.663,464.454z"></path> </g> </g> </g></svg>
                 <span className="ml-1 mt-2 sizeFont hidden group-hover:inline whitespace-nowrap">Productos</span>
               </a>
             </li>
             <li>
               <a href="#" className="flex py-2 px-3 navbar-href group-hover:px-3">
-                <svg viewBox="0 0 1024 1024" fill="#fff" className={`transition-all duration-200 w-10`}>
-                  <path d="M53.6 1023.2c-6.4 0-12.8-2.4-17.6-8-4.8-4.8-7.2-11.2-6.4-18.4L80 222.4c0.8-12.8 11.2-22.4 24-22.4h211.2v-3.2c0-52.8 20.8-101.6 57.6-139.2C410.4 21.6 459.2 0.8 512 0.8c108 0 196.8 88 196.8 196.8 0 0.8-0.8 1.6-0.8 2.4v0.8H920c12.8 0 23.2 9.6 24 22.4l49.6 768.8c0.8 2.4 0.8 4 0.8 6.4-0.8 13.6-11.2 24.8-24.8 24.8H53.6z m25.6-48H944l-46.4-726.4H708v57.6h0.8c12.8 8.8 20 21.6 20 36 0 24.8-20 44.8-44.8 44.8s-44.8-20-44.8-44.8c0-14.4 7.2-27.2 20-36h0.8v-57.6H363.2v57.6h0.8c12.8 8.8 20 21.6 20 36 0 24.8-20 44.8-44.8 44.8-24.8 0-44.8-20-44.8-44.8 0-14.4 7.2-27.2 20-36h0.8v-57.6H125.6l-46.4 726.4zM512 49.6c-81.6 0-148.8 66.4-148.8 148.8v3.2h298.4l-0.8-1.6v-1.6c0-82.4-67.2-148.8-148.8-148.8z" fill=""></path>
-                </svg>
+                <svg className={`transition-all duration-200 w-10`}  viewBox="0 0 1024 1024" fill="#fff" version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#fff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M959.018 208.158c0.23-2.721 0.34-5.45 0.34-8.172 0-74.93-60.96-135.89-135.89-135.89-1.54 0-3.036 0.06-6.522 0.213l-611.757-0.043c-1.768-0.085-3.563-0.17-5.424-0.17-74.812 0-135.67 60.84-135.67 135.712l0.188 10.952h-0.306l0.391 594.972-0.162 20.382c0 74.03 60.22 134.25 134.24 134.25 1.668 0 7.007-0.239 7.1-0.239l608.934 0.085c2.985 0.357 6.216 0.468 9.55 0.468 35.815 0 69.514-13.954 94.879-39.302 25.373-25.34 39.344-58.987 39.344-94.794l-0.145-12.015h0.918l-0.008-606.41z m-757.655 693.82l-2.585-0.203c-42.524 0-76.146-34.863-76.537-79.309V332.671H900.79l0.46 485.186-0.885 2.865c-0.535 1.837-0.8 3.58-0.8 5.17 0 40.382-31.555 73.766-71.852 76.002l-10.816 0.621v-0.527l-615.533-0.01zM900.78 274.424H122.3l-0.375-65.934 0.85-2.924c0.52-1.82 0.782-3.63 0.782-5.247 0-42.236 34.727-76.665 78.179-76.809l0.45-0.068 618.177 0.018 2.662 0.203c42.329 0 76.767 34.439 76.767 76.768 0 1.326 0.196 2.687 0.655 4.532l0.332 0.884v68.577z" fill=""></path><path d="M697.67 471.435c-7.882 0-15.314 3.078-20.918 8.682l-223.43 223.439L346.599 596.84c-5.544-5.603-12.95-8.69-20.842-8.69s-15.323 3.078-20.918 8.665c-5.578 5.518-8.674 12.9-8.7 20.79-0.017 7.908 3.07 15.357 8.69 20.994l127.55 127.558c5.57 5.56 13.01 8.622 20.943 8.622 7.925 0 15.364-3.06 20.934-8.63l244.247-244.247c5.578-5.511 8.674-12.883 8.7-20.783 0.017-7.942-3.079-15.408-8.682-20.986-5.552-5.612-12.958-8.698-20.85-8.698z" fill=""></path></g></svg>
                 <span className="ml-2 mt-2 sizeFont hidden group-hover:inline whitespace-nowrap">Órdenes</span>
               </a>
             </li>
@@ -85,8 +79,8 @@ function Dashboard() {
         {/* Menú móvil (md:hidden) */}
         <nav className={`${isMobileMenuOpen ? "block" : "hidden"} md:hidden mt-4`}>
           <ul className="space-y-3">
-            <li><a href="#" className="block px-4 py-2 text-sm navbar-href">Usuarios</a></li>
-            <li><a href="#" className="block px-4 py-2 text-sm navbar-href">Pagos</a></li>
+            <li><a href="/usuarios" className="block px-4 py-2 text-sm navbar-href">Usuarios</a></li>
+            <li><a href="/productos" className="block px-4 py-2 text-sm navbar-href">Pagos</a></li>
             <li><a href="#" className="block px-4 py-2 text-sm navbar-href">Órdenes</a></li>
             <li><a href="#" className="block px-4 py-2 text-sm navbar-href">Historial de pagos</a></li>
           </ul>
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -75,8 +75,28 @@ function Navbar() {
                       {userRole === "admin" && (
                         <a href="#" className="block px-4 py-2 text-gray-700 dropdown-select">Panel de Control</a>
                       )}
-                    </div>
-                  </div>
+                    </div> 
+                  </div>  
+                  <a
+                    href="/cart"
+                    className="group p-2  hover:text-gray-900 transition-colors"
+                  >
+                    <svg
+                      className="w-5 h-5  group-hover:text-amber-500 transition-colors duration-200"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </a>
+
                 </>
               ) : (
                 <a href="/login" className="py-4 px-3 navbar-href">Iniciar Sesión</a>
